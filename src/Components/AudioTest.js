6cb09509570cf6d94ef2f26665cccc11dd3cebf6
C. Felix Lee <felixlee.music@gmail.com>
2022-11-17 16:01:00 -0500

    audiotest

--- src/Components/AudioTest.js
+++ src/Components/AudioTest.js
@@ -6,6 +6,7 @@ let gain1 = actx.createGain();
 let out = actx.destination;
 osc1.connect(gain1);
 gain1.connect(out);
+osc1.frequency.linearRampToValueAtTime(100, actx.currentTime + 6);
 
 export default function AudioTest() {
   return (
@@ -14,7 +15,12 @@ export default function AudioTest() {
       {console.log(actx)}
       {console.log(osc1)}
       {console.log(gain1)}
+      <br />
+      {osc1.frequency.value}
+      <br />
+      {(osc1.type = "sawtooth")}
       {osc1.start()}
+      <br />
       <button onClick={() => osc1.stop()}>stop</button>
     </div>
   );
