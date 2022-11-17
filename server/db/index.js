903bc801ee4196a3d4924166f310ab4ee4aec40f
C. Felix Lee <felixlee.music@gmail.com>
2022-11-17 13:42:28 -0500

    clean up template

--- server/db/index.js
+++ server/db/index.js
@@ -1,23 +1,12 @@
 const conn = require("./conn");
 const User = require("./User");
-const Product = require("./Product");
-const Order = require("./Order");
-const LineItem = require("./LineItem");
-
-Order.belongsTo(User);
-Order.hasMany(LineItem);
-LineItem.belongsTo(Order);
-LineItem.belongsTo(Product);
 
 const syncAndSeed = async () => {
   await conn.sync({ force: true });
-  const [moe, lucy, larry, foo, bar, baz] = await Promise.all([
+  const [moe, lucy, larry] = await Promise.all([
     User.create({ username: "moe", password: "123" }),
     User.create({ username: "lucy", password: "123" }),
     User.create({ username: "larry", password: "123" }),
-    Product.create({ name: "foo" }),
-    Product.create({ name: "bar" }),
-    Product.create({ name: "baz" }),
   ]);
   const cart = await moe.getCart();
   return {
@@ -26,16 +15,10 @@ const syncAndSeed = async () => {
       lucy,
       larry,
     },
-    products: {
-      foo,
-      bar,
-      baz,
-    },
   };
 };
 
 module.exports = {
   syncAndSeed,
   User,
-  Product,
 };
